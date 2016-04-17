f7ec120840b1af74587fa16a6cbab41b0abfd089
puerapuliae <bk@puerapuliae.net>
2016-04-17 16:06:07 +0200

    newest buildroot version 2016.02

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VERSION=2015.08.1
+VERSION=2016.02
 
 rm -rf ~/buildroot
 curl http://buildroot.uclibc.org/downloads/buildroot-$VERSION.tar.bz2 | tar jx && mv buildroot-$VERSION ~/buildroot
 cp config ~/buildroot/.config
 cp busybox.config ~/buildroot/package/busybox/busybox.config
 cd ~/buildroot
-make
-sudo docker import - puerapuliae/gaia < ~/buildroot/output/images/rootfs.tar
+#make
+#sudo docker import - puerapuliae/nextos < ~/buildroot/output/images/rootfs.tar
